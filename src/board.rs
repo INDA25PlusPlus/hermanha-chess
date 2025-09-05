79377604336cf86f185955fe38356d30e7e0e32d
Herman Hallonqvist <herman.hallonqvist@gmail.com>
2025-09-05 16:56:23 +0200

    Working on #2, Board and Pieces now have their own structs for keeping track of positions and placing pieces. Will probably need changes later.

---
+++ src/board.rs
@@ -0,0 +1,76 @@
+use crate::pieces::{Piece, PieceType, Color};
+
+#[derive(Debug, Clone, Copy)] // chat thought me this :D
+pub struct Position{
+    pub row: u8,
+    pub col: u8,
+}
+
+pub struct Board{
+    squares: [[Option<Piece>; 8]; 8],
+    pub move_turn: Color,
+}
+
+impl Board{
+    pub fn new() -> Self{
+        Board{
+            squares: [[None; 8]; 8],
+            move_turn: Color::White,
+        }
+    }
+
+    pub fn get(&self, position: Position) -> Option<Piece>{
+        self.squares[position.row as usize][position.col as usize]
+    }
+
+    pub fn set(&mut self, position: Position, piece: Option<Piece>){
+        self.squares[position.row as usize][position.col as usize] = piece;
+    }
+
+    pub fn setup_standard(&mut self){
+        self.squares = [[None; 8]; 8];
+
+        // Place pawns
+        for col in 0..8 {
+            self.squares[1][col] = Some(Piece { piece_type: PieceType::Pawn, color: Color::White });
+            self.squares[6][col] = Some(Piece { piece_type: PieceType::Pawn, color: Color::Black });
+        }
+        // Place rooks
+        self.squares[0][0] = Some(Piece { piece_type: PieceType::Rook, color: Color::White });
+        self.squares[0][7] = Some(Piece { piece_type: PieceType::Rook, color: Color::White });
+        self.squares[7][0] = Some(Piece { piece_type: PieceType::Rook, color: Color::Black });
+        self.squares[7][7] = Some(Piece { piece_type: PieceType::Rook, color: Color::Black });
+
+        // Place knights
+        self.squares[0][1] = Some(Piece { piece_type: PieceType::Knight, color: Color::White });
+        self.squares[0][6] = Some(Piece { piece_type: PieceType::Knight, color: Color::White });
+        self.squares[7][1] = Some(Piece { piece_type: PieceType::Knight, color: Color::Black });
+        self.squares[7][6] = Some(Piece { piece_type: PieceType::Knight, color: Color::Black });
+
+        // Place bishops
+        self.squares[0][2] = Some(Piece { piece_type: PieceType::Bishop, color: Color::White });
+        self.squares[0][5] = Some(Piece { piece_type: PieceType::Bishop, color: Color::White });
+        self.squares[7][2] = Some(Piece { piece_type: PieceType::Bishop, color: Color::Black });
+        self.squares[7][5] = Some(Piece { piece_type: PieceType::Bishop, color: Color::Black });
+
+        // Place queens
+        self.squares[0][3] = Some(Piece { piece_type: PieceType::Queen, color: Color::White });
+        self.squares[7][3] = Some(Piece { piece_type: PieceType::Queen, color: Color::Black });
+
+        // Place kings
+        self.squares[0][4] = Some(Piece { piece_type: PieceType::King, color: Color::White });
+        self.squares[7][4] = Some(Piece { piece_type: PieceType::King, color: Color::Black });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_starting_board() {
+        let mut board = Board::new();
+        board.setup_standard();
+        println!("{:?}", board.squares)
+    }
+}
