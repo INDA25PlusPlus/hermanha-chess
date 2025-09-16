0a1b5a66e6a82cf2c446cc5e55b3b189a8059630
Herman Hallonqvist <herman.hallonqvist@gmail.com>
2025-09-16 17:49:01 +0200

    optimize movegen
    early exit if from_pos has no piece. makes tests go alot faster

--- src/movegen.rs
+++ src/movegen.rs
@@ -7,30 +7,32 @@ const PROMOTION_PIECES: [PieceType; 4] = [Queen, Rook, Bishop, Knight];
 pub fn all_legal_moves(board: &Board) -> Vec<(Position, Position, Option<PieceType>)> {
     let mut legal_moves: Vec<(Position, Position, Option<PieceType>)> = Vec::new();
 
-    
-
-    for from_row in 0..BOARD_ROWS{
+    for from_row in 0..BOARD_ROWS {
         for from_col in 0..BOARD_COLS {
             let from_pos = pos(from_row, from_col);
+            
+            if let Some(piece) = board.get(from_pos) {
+                if piece.color == board.move_turn {
+                    for to_row in 0..BOARD_ROWS {
+                        for to_col in 0..BOARD_COLS {
+                            let to_pos = pos(to_row, to_col);
+                            let mut tmp = board.clone();
 
-            for to_row in 0..BOARD_ROWS {
-                for to_col in 0..BOARD_COLS{
-                    let to_pos = pos(to_row, to_col);
-                    let mut tmp = board.clone();
-
-                    match tmp.move_piece(from_pos, to_pos, None) {
-                        Ok(MoveOk::Done) => {
-                            legal_moves.push((from_pos, to_pos, None));
-                        }
-                        Ok(MoveOk::NeedsPromotion) => {
-                            for &pp in &PROMOTION_PIECES {
-                                let mut tmp2 = board.clone();
-                                if let Ok(MoveOk::Done) = tmp2.move_piece(from_pos, to_pos, Some(pp)) {
-                                    legal_moves.push((from_pos, to_pos, Some(pp)));
+                            match tmp.move_piece(from_pos, to_pos, None) {
+                                Ok(MoveOk::Done) => {
+                                    legal_moves.push((from_pos, to_pos, None));
+                                }
+                                Ok(MoveOk::NeedsPromotion) => {
+                                    for &pp in &PROMOTION_PIECES {
+                                        let mut tmp2 = board.clone();
+                                        if let Ok(MoveOk::Done) = tmp2.move_piece(from_pos, to_pos, Some(pp)) {
+                                            legal_moves.push((from_pos, to_pos, Some(pp)));
+                                        }
+                                    }
+                                }
+                                Err(_e) => {
                                 }
                             }
-                        }
-                        Err(_e) => {
                         }
                     }
                 }
